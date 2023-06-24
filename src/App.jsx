2f6943aa10c0ca97437a3db44f79dbdc01296609
Shadow <Nayem58@users.noreply.github.com>
2023-06-24 11:56:27 +0600

    installed react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import Header from "./layouts/Header";
 import Footer from "./layouts/Footer";
 
 function App() {
+  const consoleLog = () => {
+    console.log("clicked");
+  };
   return (
     <>
       <Header />
+      <button onClick={consoleLog}>Click Me</button>
       <Footer />
     </>
   );
