e9cf4c6632b7df96618bd75b16e3f5c4b26b85f3
Shadow <Nayem58@users.noreply.github.com>
2023-06-24 12:26:14 +0600

    looping through items and displaying them

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
+// icons
+import { BsFillTrash3Fill } from "react-icons/bs";
+
+// layouts
 import Header from "./layouts/Header";
 import Footer from "./layouts/Footer";
 
+// useState
+import { useState } from "react";
+
 function App() {
-  const consoleLog = () => {
-    console.log("clicked");
-  };
+  const [items, setItems] = useState([
+    { id: 1, item: "sauce", checked: false },
+    { id: 2, item: "coke", checked: false },
+    { id: 3, item: "noodles", checked: false },
+  ]);
+
+  items.map((item) => {
+    console.log(item.item);
+  });
+
   return (
     <>
       <Header />
-      <button onClick={consoleLog}>Click Me</button>
+      <div className="py-50px">
+        <div className="container">
+          {items.length ? (
+            <ul className="groceries-list">
+              {items.map((item) => {
+                return (
+                  <li className="groceries-list__item" key={item.id}>
+                    <input className="checkbox" type="checkbox" id={item.id} />
+                    <label className="label" htmlFor={item.id}>
+                      {item.item.toUpperCase()}
+                    </label>
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            <p className="text-center">Your List Is Empty</p>
+          )}
+        </div>
+      </div>
       <Footer />
     </>
   );
